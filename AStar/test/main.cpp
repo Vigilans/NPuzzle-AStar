2d64f42506184d50e494025559ece31848c04179
Vigilans <vigilans@foxmail.com>
2019-05-28 01:35:26 +0800

    Add test main file

---
+++ AStar/test/main.cpp
@@ -0,0 +1,36 @@
+#include "../module/AStar.hpp"
+#include "../module/NPuzzle.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    constexpr size_t N = 5;
+    using Problem = NPuzzle<N>;
+    using State = Problem::State;
+
+    //for (const auto& board : State::Scrambled(20, true)) {
+    //    cout << board << endl;
+    //}
+
+    auto problem = Problem{ &HammingDistance<N> };
+    //auto problem = Problem{ &ManhattanDistance<N> };
+    auto astar = AStarSearch{ problem };
+
+    auto states = State::Scrambled(30, true);
+    auto start = states.back();
+    //auto start = State::Random();
+    
+    auto path = astar.findPath(start, State::Ordered());
+
+    for (const auto& board : path) {
+        cout << board << endl;
+    }
+
+    cout << "Path length: " << path.size() << endl;
+    cout << "States explorered: " << astar.statesExplorered() << endl;
+
+    astar.clear();
+
+    return 0;
+}
